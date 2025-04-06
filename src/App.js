255d04a3b6172c30d1935846e11d2c34e67a2042
yabloko25 <abubakrkhujaev25@gmail.com>
2025-04-06 14:55:29 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useReducer } from 'react';
+import './App.css';
+
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    case 'reset':
+      return { count: 0 };
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className="App">
+      <h1>Counter: {state.count}</h1>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+    </div>
+  );
+}
+
+export default App;
